cec0a790147b4d629da443bdebd0007c3a2d33be
CristianAcostaDuarte <101611537+CristianAcostaDuarte@users.noreply.github.com>
2022-08-07 09:08:29 -0500

    printing the nodes

--- Solution/src/App.java
+++ Solution/src/App.java
@@ -20,7 +20,117 @@ public class App {
 
             }
             sc.close();
-            System.out.println(array[0].length);
+            
+            // creando nodos.
+            ArrayList<Node> NodeList = new ArrayList<Node>();
+
+            System.out.println("---------PRINTING NODES-----------");
+            for (int i=0; i < array.length ; i++){
+                for (int j=0; j < array[0].length; j++){
+                    Node pivot = new Node();
+    
+                    if (i==0 & j==0){
+                        pivot.setId1(i);
+                        pivot.setId2(j);
+                        pivot.setValue(array[i][j]);
+                        pivot.setUp(-1, -1, -1);
+                        pivot.setDown(i+1, j, array[i+1][j]);
+                        pivot.setLeft(-1, -1, -1);
+                        pivot.setRight(i, j+1, array[i][j+1]);
+    
+                    }else if (i==0 & j==array[0].length-1){
+                        pivot.setId1(i);
+                        pivot.setId2(j);
+                        pivot.setValue(array[i][j]);
+                        pivot.setUp(-1, -1, -1);
+                        pivot.setDown(i+1, j, array[i+1][j]);
+                        pivot.setLeft(i, j-1, array[i][j-1]);
+                        pivot.setRight(-1, -1, -1);
+    
+                    }else if (i==array.length-1 & j==0){
+                        pivot.setId1(i);
+                        pivot.setId2(j);
+                        pivot.setValue(array[i][j]);
+                        pivot.setUp(i-1, j, array[i-1][j]);
+                        pivot.setDown(-1, -1, -1);
+                        pivot.setLeft(-1, -1,-1);
+                        pivot.setRight(i, j+1, array[i][j+1]);
+    
+                    }else if (i==array.length-1 & j==array[0].length-1){
+                        pivot.setId1(i);
+                        pivot.setId2(j);
+                        pivot.setValue(array[i][j]);
+                        pivot.setUp(i-1, j, array[i-1][j]);
+                        pivot.setDown(-1, -1, -1);
+                        pivot.setLeft(i, j-1, array[i][j-1]);
+                        pivot.setRight(-1, -1, -1);
+    
+                    }else if (i==0 & 0<j & j<array[0].length-1){
+                        pivot.setId1(i);
+                        pivot.setId2(j);
+                        pivot.setValue(array[i][j]);
+                        pivot.setUp(-1, -1, -1);
+                        pivot.setDown(i+1, j, array[i+1][j]);
+                        pivot.setLeft(i, j-1, array[i][j-1]);
+                        pivot.setRight(i, j+1, array[i][j+1]);
+    
+                    }else if (j==0 & 0<i & i<array.length-1){
+                        pivot.setId1(i);
+                        pivot.setId2(j);
+                        pivot.setValue(array[i][j]);
+                        pivot.setUp(i-1, j, array[i-1][j]);
+                        pivot.setDown(i+1, j, array[i+1][j]);
+                        pivot.setLeft(-1, -1, -1);
+                        pivot.setRight(i, j+1, array[i][j+1]);
+    
+                    }else if (j==array[0].length-1 &0<i & i<array.length-1){
+                        pivot.setId1(i);
+                        pivot.setId2(j);
+                        pivot.setValue(array[i][j]);
+                        pivot.setUp(i-1, j, array[i-1][j]);
+                        pivot.setDown(i+1, j, array[i+1][j]);
+                        pivot.setLeft(i, j-1, array[i][j-1]);
+                        pivot.setRight(-1, -1, -1);
+    
+                    }else if (i==array.length-1 &  0<j & j<array[0].length-1){
+                        pivot.setId1(i);
+                        pivot.setId2(j);
+                        pivot.setValue(array[i][j]);
+                        pivot.setUp(i-1, j, array[i-1][j]);
+                        pivot.setDown(-1, -1, -1);
+                        pivot.setLeft(i, j-1, array[i][j-1]);
+                        pivot.setRight(i, j+1, array[i][j+1]);
+    
+                    }else{
+    
+                        pivot.setId1(i);
+                        pivot.setId2(j);
+                        pivot.setValue(array[i][j]);
+                        pivot.setUp(i-1, j, array[i-1][j]);
+                        pivot.setDown(i+1, j, array[i+1][j]);
+                        pivot.setLeft(i, j-1, array[i][j-1]);
+                        pivot.setRight(i, j+1, array[i][j+1]);
+    
+    
+                    }
+    
+                    
+                   NodeList.add(pivot);
+    
+               }
+           }
+    
+           System.out.println("The size of the ArrayList is: " + NodeList.size());
+    
+    
+            for(Node pd : NodeList) {
+                System.out.println(pd);
+            }
+
+
+
+
+
         }
 
         catch (Exception e) {
